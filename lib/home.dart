afdd94876b097d356a725035686207a1a5708689
mahnoorkhursheed54 <64984767+mahnoorkhursheed54@users.noreply.github.com>
2021-06-28 18:21:23 +0500

    Add files via upload

---
+++ lib/home.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class Calculator extends StatefulWidget {
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+
+var result= "";
+
+Widget btn(var textt){
+  return ElevatedButton(onPressed: (){
+    setState(() {
+      result = result + textt;
+    });
+  }, child: Text(textt));
+}
+
+clearr(){
+  setState(() {
+    result = "";
+  });
+}
+
+output(){
+  Parser p = Parser();
+  Expression exp = p.parse(result);
+  ContextModel cm = ContextModel();
+  double eval = exp.evaluate(EvaluationType.REAL, cm);
+
+  setState((){
+    result = eval.toString();
+  });
+
+}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text ("Calculator"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children:[
+            Text(result, style: TextStyle(
+              fontSize: 30, fontWeight: FontWeight.bold, 
+            ),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              btn("1"),
+              btn("2"),
+              btn("3"),
+              btn("4"),
+            ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              btn("5"),
+              btn("6"),
+              btn("7"),
+              btn("8"),
+            ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              btn("9"),
+              btn("0"),
+              btn("+"),
+              btn("-"),
+            ],),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+              btn("*"),
+              btn("/"),
+              ElevatedButton(onPressed: clearr, child: Text("clear")),
+              ElevatedButton(onPressed: output, child: Text("=")),
+            ],),
+          ]
+        ),
+      )
+    );
+  }
+}
